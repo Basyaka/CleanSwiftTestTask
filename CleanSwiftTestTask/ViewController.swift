63fa8fff640eb8766d14d8ccc3623ded8085a3f0
Vlad Novik <imvladnovik@gmail.com>
2022-08-12 13:49:22 +0300

    Initial Commit

---
+++ CleanSwiftTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSwiftTestTask
+//
+//  Created by Vlad Novik on 12.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
